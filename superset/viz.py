2c4adefa81292885236ef3b3074e10762fb153b0
Full Stack Web Developer <josef779@outlook.com>
2019-08-04 15:06:48 -0700

    DeckScreengrid

--- superset/viz.py
+++ superset/viz.py
@@ -2641,29 +2641,29 @@ class DeckScatterViz(BaseDeckGLViz):
         return super(DeckScatterViz, self).get_data(df)
 
 
-class DeckScreengrid(BaseDeckGLViz):
-    """deck.gl's ScreenGridLayer"""
-
-    viz_type = 'deck_screengrid'
-    verbose_name = _('Deck.gl - Screen Grid')
-    spatial_control_keys = ['spatial']
-    is_timeseries = True
-
-    def query_obj(self):
-        fd = self.form_data
-        self.is_timeseries = fd.get('time_grain_sqla') or fd.get('granularity')
-        return super(DeckScreengrid, self).query_obj()
-
-    def get_properties(self, d):
-        return {
-            'position': d.get('spatial'),
-            'weight': d.get(self.metric_label) or 1,
-            '__timestamp': d.get(DTTM_ALIAS) or d.get('__time'),
-        }
-
-    def get_data(self, df):
-        self.metric_label = utils.get_metric_name(self.metric)
-        return super(DeckScreengrid, self).get_data(df)
+# class DeckScreengrid(BaseDeckGLViz):
+#     """deck.gl's ScreenGridLayer"""
+#
+#     viz_type = 'deck_screengrid'
+#     verbose_name = _('Deck.gl - Screen Grid')
+#     spatial_control_keys = ['spatial']
+#     is_timeseries = True
+#
+#     def query_obj(self):
+#         fd = self.form_data
+#         self.is_timeseries = fd.get('time_grain_sqla') or fd.get('granularity')
+#         return super(DeckScreengrid, self).query_obj()
+#
+#     def get_properties(self, d):
+#         return {
+#             'position': d.get('spatial'),
+#             'weight': d.get(self.metric_label) or 1,
+#             '__timestamp': d.get(DTTM_ALIAS) or d.get('__time'),
+#         }
+#
+#     def get_data(self, df):
+#         self.metric_label = utils.get_metric_name(self.metric)
+#         return super(DeckScreengrid, self).get_data(df)
 
 
 class DeckGrid(BaseDeckGLViz):
